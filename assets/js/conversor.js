0a7d59c04379b8a819b040a86bb877357ee9ce3a
Tania González Rodríguez <ainatrg@gmail.com>
2016-03-27 22:04:41 +0100

    Creadas las funciones de cambio de temperaturas. Sin comprobar

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -1,25 +1,56 @@
 (function(exports) {
   "use strict";
 
-  function Medida(valor,tipo)  
+  function validar(expresion) {
+  
+  }
+
+  function Medida(valor,tipo)
   {
     /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
     /* ademas de new Medida(45.2, "Km") */
+    this.valor = valor || 0;
+    this.tipo = tipo || "vacio";
+    console.log("medida");
   }
-  
+
   function Temperatura(valor,tipo)
   {
     /* tipo es opcional. Debería admitir new Medida("45.2 F") */
+    Medida.call(this,valor,tipo);
+
   }
-  
+  Temperatura.prototype = new Medida();
+  Temperatura.prototype.constructor = Temperatura;
+
+
   function Celsius(valor)
   {
+    Temperatura.call(this, valor);
   }
-  
+  Celsius.prototype = new Temperatura();
+  Celsius.prototype.constructor = Celsius;
+  Celsius.prototype.toFarenheit = function() {
+    return ((this.value * 9/5) + 32);
+  };
+
+  Celsius.prototype.toKelvin = function() {
+    return (this.value + 273.15);
+  };
+
   function Farenheit(valor)
   {
+    Temperatura.call(this, valor);
   }
-  
+  Farenheit.prototype = new Temperatura();
+  Farenheit.prototype.constructor = Farenheit;
+  Farenheit.prototype.toCelsius = function(){
+    return ((this.value - 32)* 5/9);
+  };
+  Farenheit.prototype.toKelvin = function(){
+    return (((this.value - 32)*5/9) + 273);
+  };
+
   exports.Temperatura = Temperatura;
   exports.Celsius = Celsius;
   exports.Farenheit = Farenheit;
@@ -30,14 +61,14 @@
         /* Extienda la RegeExp a la especificación. use una XRegExp */
         regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([a-z,A-Z]+)\s*$/i;
     valor     = valor.match(regexp);
-    
+
     if (valor) {
       var numero = valor[1],
           tipo   = valor[2].toLowerCase();
-      
+
       numero = parseFloat(numero);
       console.log("Valor: " + numero + ", Tipo: " + tipo);
-      
+
       switch (tipo) {
         case 'c':
           var celsius = new Celsius(numero);
@@ -47,9 +78,9 @@
           var farenheit = new Farenheit(numero);
           elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
           break;
-        
+
         default:
-          /* rellene este código */
+          elemento.innerHTML = "Error! Parace haber un error en la entrada. Inténtelo de nuevo"
       }
     }
     else
